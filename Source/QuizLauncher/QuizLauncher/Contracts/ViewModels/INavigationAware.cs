af73822645149efecdc9e8d86e7fe4af3fe87dcf
ombolics <omblics@gmail.com>
2022-02-19 16:13:54 +0100

    GameLauncher Added

---
+++ Source/QuizLauncher/QuizLauncher/Contracts/ViewModels/INavigationAware.cs
@@ -0,0 +1,9 @@
+﻿namespace QuizLauncher.Contracts.ViewModels
+{
+    public interface INavigationAware
+    {
+        void OnNavigatedTo(object parameter);
+
+        void OnNavigatedFrom();
+    }
+}
